a8101606104f0d7b8e0864ff86413f21de1636df
Jeet Inder <jashansingh4000@gmail.com>
2026-04-06 22:21:31 +0530

    Added search functionality

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const { GoogleGenAI } = require("@google/genai");
 
 require("dotenv").config();
 
 const app = express();
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+});
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Server is running");
@@ -14,17 +19,52 @@ app.get("/", (req, res) => {
 
 app.get("/news", async (req, res) => {
     try {
-        const response = await axios.get(
-            `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
-        );
+
+        const query = req.query.q;
+        let url = "";
+
+        if (query) {
+            url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.NEWS_API_KEY}`;
+        } else {
+            url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`;
+        }
+
+        const response = await axios.get(url);
         res.json(response.data);
+
     } catch (error) {
-        console.error(error.message);
+
+        console.error(error.response?.data || error.message);
         res.status(500).json({ error: "Failed to fetch news" });
+
     }
 });
 
-const PORT = 5000;
+app.post("/summarise", async (req, res) => {
+    try {
+        const { title, description } = req.body;
+
+        const prompt = `
+        Summarise this news article in exactly 2 concise sentences:
+        
+        Title: ${title}
+        Description: ${description}
+        `;
+
+        const response = await ai.models.generateContent({
+            model: "gemini-3-flash-preview",
+            contents: prompt
+        });
+
+        res.json({summary: response.text});
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "AI summarisation failed" });
+    }
+})
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,57 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
+import axios from 'axios';
+
 function App() {
-  const [news, setNews] = useState([])
+  const [ news, setNews ] = useState([]);
+  const [ searchTerm, setSearchTerm ] = useState("");
+  const [ summaries, setSummaries ] = useState({});
+
+  const fetchNews = async (query = "") => {
+    try {
+      let url = "";
+
+      if (query) {
+        url = `http://localhost:5000/news?q=${query}`;
+      } else {
+        url = "http://localhost:5000/news";
+      }
+
+      const response = await axios.get(url);
+      setNews(response.data.articles);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleSearch = () => {
+    fetchNews(searchTerm.trim());
+  };
+
+  const getSummary = async (article, index) => {
+    try {
+      const res = await axios.post("http://localhost:5000/summarise", {
+
+        title: article.title,
+        description: article.description
+
+      });
+
+      setSummaries((prev) => ({
+        ...prev,
+        [index]: res.data.summary
+      }))
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
-    fetch("http://localhost:5000/news")
-      .then((res) => res.json())
-      .then((data) => {
-        setNews(data.articles);
-      })
-      .catch((err) => console.error(err));
+
+    fetchNews()
   }, []);
 
   return (
@@ -21,16 +59,51 @@ function App() {
       <h1>
         NovaNews
       </h1>
+
+      <input
+        type="text"
+        placeholder="Search news..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleSearch();
+        }}
+      />
+
+      <button onClick={handleSearch}>
+        Search
+      </button>
+
       {news.map((article, index) => {
           return (
             <div key={index}>
+
+              {/* Comment: showing the image */}
+
               {article.urlToImage && (
-                <img src={article.urlToImage} alt="news" style={{width: "250px", height: "auto"}} />
+                <img src={article.urlToImage} alt={article.title} style={{width: "250px", height: "auto"}} loading="lazy" />
               )}
+
+              {/* Comment: making the title into a button to go to full article */}
+
               <a href={article.url} target="_blank" rel="noopener noreferrer">
                 <h3>{article.title}</h3>
               </a>
+
+              {/* Comment: just showing the desription of the article */}
+
               <p>{article.description}</p>
+
+              {/* Comment: AI Summary button and showing the summary */}
+
+              <button onClick={() => getSummary(article, index)}>
+                Get AI Summary
+              </button>
+
+              {summaries[index] && (
+                <p>AI Summary: {summaries[index]}</p>
+              )}
+
             </div>
           );
         })}
